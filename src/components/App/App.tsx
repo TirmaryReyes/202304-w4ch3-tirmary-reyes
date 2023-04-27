f474a1c4f99b576cfba484aa38bc3ede004a340d
Tirmary Reyes <tmrv1991@gmail.com>
2023-04-27 21:35:56 +0200

    Fix with info component App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,21 +1,7 @@
 import Info from "../Info/Info";
-import Key from "../Key/Key";
 
 const App = (): JSX.Element => {
-  const isCalling = true;
-  return (
-    <>
-      <Info isCalling={isCalling} />
-      <Key
-        text={"1"}
-        isBig={false}
-        isDisabled={false}
-        actionOnClick={function (): void {
-          throw new Error("Function not implemented.");
-        }}
-      ></Key>
-    </>
-  );
+  return <Info isCalling />;
 };
 
 export default App;
